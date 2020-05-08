0866e4f011889f27b2204f388a349996006fc643
Vlarge Empire <vlargeempire@gmail.com>
2020-05-08 11:54:57 +0100

    Serve static files, add pages, and use template inheritance

--- BasicProject/HelloDjangoApp/views.py
+++ BasicProject/HelloDjangoApp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
-
+from datetime import datetime
 def index(request):
     now = datetime.now()
 
@@ -10,24 +9,20 @@ def index(request):
         "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
         # "index.html", # Use this code for VS 2017 15.7 and earlier
         {
-            'content': "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+            'title' : "Hello Django",
+            'message' : "Hello Django!",
+            'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
         }
     )
 
-
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello, Django!")
-
-from datetime import datetime
-
-def index(request):
-    now = datetime.now()
-
-    html_content = "<html><head><title>Hello, Django</title></head><body>"
-    html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
-    html_content += "</body></html>"
-
-    return HttpResponse(html_content)
+def about(request):
+    return render(
+        request,
+        "HelloDjangoApp/about.html",
+        {
+            'title' : "About HelloDjangoApp",
+            'content' : "Example app page for Django."
+        }
+    )
+# Create your views here.
 
